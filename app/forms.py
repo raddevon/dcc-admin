003911d62834dd3543e457d01bedc4d4690ceeaa
Devon Campbell <raddevon@gmail.com>
2013-07-31 09:21:43 -0400

    New signup form

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,14 @@
 from flask.ext.wtf import Form
 from wtforms import TextField, TextAreaField, BooleanField, PasswordField, \
-                    SubmitField, validators, ValidationError
+    SubmitField, validators, ValidationError
 from models import User
 
 
 class SignupForm(Form):
-    email = TextField('Email', [validators.Required('Please enter your email address.'), validators.Email('Enter a valid email address.')])
-    password = PasswordField('Password', [validators.Required('Please enter a password.')])
+    email = TextField(
+        'Email', [validators.Required('Please enter your email address.'), validators.Email('Enter a valid email address.')])
+    password = PasswordField(
+        'Password', [validators.Required('Please enter a password.')])
     submit = SubmitField('Create user')
 
     def __init__(self, *args, **kwargs):
@@ -16,12 +18,14 @@ class SignupForm(Form):
         if not Form.validate(self):
             return False
 
-        user = User.query.filter_by(email = self.email.data.lower()).first()
+        user = User.query.filter_by(email=self.email.data.lower()).first()
         if user:
-            self.email.errors.append('That email is already attached to an existing user.')
+            self.email.errors.append(
+                'That email is already attached to an existing user.')
             return False
         else:
             return True
+
 
 class LoginForm(Form):
     username = TextField('username', [validators.Required()])
