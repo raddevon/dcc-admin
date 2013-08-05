5617b4f352a6110afbc976e98f4e300f737f1e90
Devon Campbell <raddevon@gmail.com>
2013-08-05 13:13:57 -0400

    User logins and link visibility

--- app/views.py
+++ app/views.py
@@ -3,29 +3,72 @@ from app import app
 from forms import LoginForm, SignupForm
 from models import db, User
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('home.html')
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
 
+    if 'email' in session:
+        return redirect(url_for('profile'))
+
     if request.method == 'POST':
-        if form.validate() == False:
+        if not form.validate():
             return render_template('signup.html', form=form)
         else:
             newuser = User(form.email.data, form.password.data)
             db.session.add(newuser)
             db.session.commit()
-            return 'User created successfully'
+
+            session['email'] = newuser.email
+
+            return redirect(url_for('profile'))
+
     elif request.method == 'GET':
         return render_template('signup.html', form=form)
 
-@app.route('/login', methods = ['GET', 'POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html',
-        title = 'Sign in',
-        form = form)
+
+    if 'email' in session:
+        return redirect(url_for('profile'))
+
+    if request.method == 'POST':
+        if not form.validate():
+            return render_template('login.html',
+                                   title='Login',
+                                   form=form)
+        else:
+            session['email'] = form.email.data
+            return redirect(url_for('profile'))
+    elif request.method == 'GET':
+        return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    if 'email' not in session:
+        return redirect(url_for('login'))
+
+    session.pop('email', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/profile')
+def profile():
+    if 'email' not in session:
+        return redirect(url_for('signin'))
+
+    user = User.query.filter_by(email=session['email']).first()
+
+    if not user:
+        return redirect(url_for('signin'))
+    else:
+        return render_template('profile.html')
