bb7b215a7510ae8ae28fc3e26b4a7d790e87fd91
Devon Campbell <raddevon@gmail.com>
2013-08-12 08:56:39 -0400

    Initialize login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask.ext.login import LoginManager
 from config import DATABASE_URI
 from models import db
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+# Flask-Login
+login_manager = LoginManager()
+login_manager(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 db.init_app(app)
