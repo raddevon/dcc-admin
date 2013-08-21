315c53c7a99fae1075f732f3b195a98205d9b549
Devon Campbell <raddevon@gmail.com>
2013-08-21 12:33:16 -0400

    Make list of abilities to check against

--- app/auth.py
+++ app/auth.py
@@ -17,7 +17,10 @@ def user_has(attribute):
         def inner(*args, **kwargs):
             attribute_object = Role.query.filter_by(name=attribute).first() or \
                 Ability.query.filter_by(name=attribute).first()
-            if attribute_object in current_user.roles or attribute_object in current_user.roles.abilities.all():
+            user_abilities = []
+            for role in current_user.roles:
+                user_abilities += [role.ability for ability in role.abilities]
+            if attribute_object in current_user.roles or attribute_object in user_abilities:
                 return func(*args, **kwargs)
             else:
                 # Make this do someting way better.
