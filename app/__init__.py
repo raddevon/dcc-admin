21a04fb5fad67a58a4328cb9f0b1b48d8a138f3a
Devon Campbell <raddevon@gmail.com>
2013-09-03 08:37:43 -0400

    Establishes Flask-Permissions object

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.ext.login import LoginManager
 from config import DATABASE_URI
 from models import db
+from app.flask_permissions import Permissions
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -9,6 +10,9 @@ app.config.from_object('config')
 # Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+# Flask-Permissions
+perms = Permissions(app, db)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 db.init_app(app)
