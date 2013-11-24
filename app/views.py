badadcaa4a101f5cf4fc0a0d6d4488604e47739b
Devon Campbell <raddevon@gmail.com>
2013-11-24 12:54:24 -0500

    Adds new master template path

--- app/views.py
+++ app/views.py
@@ -11,8 +11,8 @@ from flask.ext.permissions.decorators import user_is, user_has
 @app.route('/')
 def index():
     if app.debug:
-        return make_response(open('app/static/partials/index.html').read())
-    return send_file('app/static/partials/index.html')
+        return make_response(open('app/templates/index.html').read())
+    return send_file('app/templates/index.html')
 
 
 @app.route('/profile')
