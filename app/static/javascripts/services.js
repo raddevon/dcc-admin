9db4b434be33326dcf5fe4f827551f6640c81dab
Devon Campbell <raddevon@gmail.com>
2013-11-25 19:36:44 -0500

    Removes optional parameters

--- app/static/javascripts/services.js
+++ app/static/javascripts/services.js
@@ -1,9 +1,4 @@
 angular.module('dccAdminServices', ['ngResource'])
         .factory('Users', function($resource) {
-                return $resource('/api/user/', {}, {
-                        query: {
-                                method: 'GET',
-                                isArray: true
-                        }
-                });
+                return $resource('/api/user/');
         });
