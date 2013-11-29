3e3026515ed121301b47e2843580823244b2e77a
Devon Campbell <raddevon@gmail.com>
2013-11-29 12:33:55 -0500

    Adds roles factory

--- app/static/javascripts/services.js
+++ app/static/javascripts/services.js
@@ -1,4 +1,7 @@
 angular.module('dccAdminServices', ['ngResource'])
         .factory('Users', function($resource) {
                 return $resource('/api/user/');
+        })
+        .factory('Roles', function($resource) {
+                return $resource('/api/role/');
         });
