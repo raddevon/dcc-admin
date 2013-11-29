8da377463c8909d2682ead062940495778e41f04
Devon Campbell <raddevon@gmail.com>
2013-11-29 12:33:46 -0500

    Adds roles controller

--- app/static/javascripts/controllers.js
+++ app/static/javascripts/controllers.js
@@ -1,7 +1,10 @@
-function userManagementController($scope, $http, Users) {
+function userManagementController($scope, $http, Users, Roles) {
     var usersQuery = Users.get({}, function(users) {
         $scope.users = users;
     });
+    var rolesQuery = Roles.get({}, function(roles) {
+        $scope.roles = roles;
+    })
 }
 
 function indexController($scope) {
