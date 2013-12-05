b66f8a75fd9f1adbeea17cadec50deaf7920fe98
Devon Campbell <raddevon@gmail.com>
2013-12-05 10:20:10 -0500

    Adds logic for selecting current user roles

--- app/static/javascripts/controllers.js
+++ app/static/javascripts/controllers.js
@@ -1,10 +1,20 @@
 function userManagementController($scope, $http, Users, Roles) {
     var usersQuery = Users.get({}, function(users) {
         $scope.users = users;
+
+        // Selects current user roles
+        for (var key in $scope.users) {
+            var user = $scope.users[key];
+            user.selectedRoles = [];
+            for (var roleKey in user.roles) {
+                user.selectedRoles.push(String(user.roles[roleKey].id));
+            }
+        }
+
     });
     var rolesQuery = Roles.get({}, function(roles) {
         $scope.roles = roles;
-    })
+    });
 }
 
 function indexController($scope) {
