d97327679ba8ba6f1508ce4d698061404b5cde7b
Devon Campbell <raddevon@gmail.com>
2013-12-05 10:26:06 -0500

    Comments out views Flask route

--- app/views.py
+++ app/views.py
@@ -23,30 +23,30 @@ def profile():
     return redirect(url_for('login'))
 
 
-@app.route('/users', methods=['GET', 'POST'])
-@user_is('admin')
-def admin():
-    with app.test_client() as api:
-        users = json.loads(api.get('api/users/').data)
-    forms = {uid: RoleForm(formdata=None, id=uid, roles=[role['id'] for role in user['roles']])
-             for uid, user in users.iteritems()}
-    print forms['3'].hidden_tag()
+# @app.route('/users', methods=['GET', 'POST'])
+# @user_is('admin')
+# def admin():
+#     with app.test_client() as api:
+#         users = json.loads(api.get('api/users/').data)
+#     forms = {uid: RoleForm(formdata=None, id=uid, roles=[role['id'] for role in user['roles']])
+#              for uid, user in users.iteritems()}
+#     print forms['3'].hidden_tag()
 
-    if request.method == "POST":
-        current_id = int(request.form['id'])
-        forms[current_id] = RoleForm(
-            id=current_id, roles=[(role['id'] for role in user.roles) for user in users if uid == current_id])
-        current_form = forms[current_id]
+#     if request.method == "POST":
+#         current_id = int(request.form['id'])
+#         forms[current_id] = RoleForm(
+#             id=current_id, roles=[(role['id'] for role in user.roles) for user in users if uid == current_id])
+#         current_form = forms[current_id]
 
-        if current_form.validate():
-            u = models.User.query.get(current_form.id.data)
-            u.roles = [Role.query.get(role)
-                       for role in current_form.roles.data]
-            db.session.commit()
-            flash('Roles updated for {}'.format(u))
-            return redirect(url_for('admin'))
+#         if current_form.validate():
+#             u = models.User.query.get(current_form.id.data)
+#             u.roles = [Role.query.get(role)
+#                        for role in current_form.roles.data]
+#             db.session.commit()
+#             flash('Roles updated for {}'.format(u))
+#             return redirect(url_for('admin'))
 
-    return render_template('admin.html', users=users, forms=forms)
+#     return render_template('admin.html', users=users, forms=forms)
 
 
 @app.route('/nodes', methods=['GET', 'POST'])
