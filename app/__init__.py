533c4bbf378fe7b28b01ccc4069ae580c5bf79d4
Devon Campbell <raddevon@gmail.com>
2014-01-03 12:53:48 -0500

    Refactored configuration to configure from object

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,11 @@ from config import DATABASE_URI
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.permissions.core import Permissions
 
-db = SQLAlchemy()
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object('config.Config')
+
+db = SQLAlchemy(app)
 
 # Flask-Login
 login_manager = LoginManager()
@@ -15,10 +16,5 @@ login_manager.init_app(app)
 
 # Flask-Permissions
 perms = Permissions(app, db, current_user)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
-db.init_app(app)
-with app.test_request_context():
-    db.create_all()
 
 from app import views, auth, api
--- config.py
+++ config.py
@@ -6,3 +6,27 @@ CSRF_ENABLED = True
 SECRET_KEY = os.environ['SECRET_KEY']
 DATABASE_URI = os.environ['DATABASE_URI']
 MIGRATE_REPO = os.path.join(basedir, 'migrations')
+
+import os
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.environ['SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    # SQLALCHEMY_DATABASE_URI = os.environ['DEV_DATABASE_URI']
+
+
+class TestingConfig(Config):
+    CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = os.environ['TEST_DATABASE_URI']
